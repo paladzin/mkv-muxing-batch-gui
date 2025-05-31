5eab34388ddf78f44d5d71af81c3311e4702cf4a
Andrew <paladzin@gmail.com>
2025-06-01 00:03:42 +0300

    Update SingleDefaultPresetsData.py

--- packages/Widgets/SingleDefaultPresetsData.py
+++ packages/Widgets/SingleDefaultPresetsData.py
@@ -1,17 +1,17 @@
 class SingleDefaultPresetsData:
     def __init__(self):
         self.Default_Video_Extensions = ["MKV"]
-        self.Default_Subtitle_Extensions = ["ASS"]
-        self.Default_Subtitle_Language = "English"
+        self.Default_Subtitle_Extensions = ["ASS","SRT"]
+        self.Default_Subtitle_Language = "Russian"
         self.Default_Chapter_Extensions = ["XML"]
-        self.Default_Audio_Extensions = ["AAC"]
-        self.Default_Audio_Language = "English"
+        self.Default_Audio_Extensions = ["MKA"]
+        self.Default_Audio_Language = "Russian"
         self.Default_Video_Directory = ""
         self.Default_Subtitle_Directory = ""
         self.Default_Chapter_Directory = ""
         self.Default_Audio_Directory = ""
         self.Default_Attachment_Directory = ""
         self.Default_Destination_Directory = ""
-        self.Default_Favorite_Subtitle_Languages = ['English', 'Arabic']
-        self.Default_Favorite_Audio_Languages = ['English', 'Arabic']
+        self.Default_Favorite_Subtitle_Languages = ['Russian']
+        self.Default_Favorite_Audio_Languages = ['Russian']
         self.Preset_Name = "Preset #1"
